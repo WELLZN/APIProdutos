1363a59e60002a285124fb9eef1dd2e933e5ab7d
wellzn <wellithuglife@gmail.com>
2023-09-28 17:44:03 -0300

    Atualizando API2

--- cadastroDeProdutos/src/main/java/br/com/luan/controller/ProdutoController.java
+++ cadastroDeProdutos/src/main/java/br/com/luan/controller/ProdutoController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -65,4 +66,13 @@ public class ProdutoController {
 		}).orElse(ResponseEntity.notFound().build());
 	}
 	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> deletarProduto(@PathVariable Long id) {
+		return produtoRepository.findById(id).map (objetoGravado -> { produtoRepository.deleteById(id);
+		
+	return ResponseEntity.noContent().<Void>build();
+		}).orElse(ResponseEntity.notFound().build());
+	}
+	
+	 	
 }
